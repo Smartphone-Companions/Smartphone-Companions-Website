14aa3e4fd7e45f55c60e16c3d9ac1d3475ace3c2
Martin Hertig <martin.hertig@gmail.com>
2020-04-30 10:56:55 +0200

    Products to resources

--- src/js/cms.js
+++ src/js/cms.js
@@ -7,7 +7,7 @@ import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../c
 import HomePreview from "./cms-preview-templates/home";
 import PostPreview from "./cms-preview-templates/post";
 import CompanionPreview from "./cms-preview-templates/post";
-import ProductsPreview from "./cms-preview-templates/products";
+import ResourcesPreview from "./cms-preview-templates/Resources";
 import ValuesPreview from "./cms-preview-templates/values";
 import ContactPreview from "./cms-preview-templates/contact";
 
@@ -15,7 +15,7 @@ CMS.registerPreviewStyle(styles, { raw: true });
 CMS.registerPreviewTemplate("home", HomePreview);
 CMS.registerPreviewTemplate("post", PostPreview);
 CMS.registerPreviewTemplate("companion", CompanionPreview);
-CMS.registerPreviewTemplate("products", ProductsPreview);
+CMS.registerPreviewTemplate("resources", ResourcesPreview);
 CMS.registerPreviewTemplate("values", ValuesPreview);
 CMS.registerPreviewTemplate("contact", ContactPreview);
 CMS.init();
