2388d96dfa733e8228f27b0c5796688a5d1aba5a
Martin Hertig <martin.hertig@gmail.com>
2020-05-06 11:34:08 +0200

    add button to show build instructions

--- src/index.js
+++ src/index.js
@@ -2,5 +2,17 @@
 
 import "./css/main.css";
 
-// Say hello
-console.log("🦊 Hello! Edit me in src/index.js");
+
+
+function showBuildInstructions() {
+  var x = document.getElementById("build-instructions");
+  var y = document.getElementById("build-button");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+console.log("hi there")
