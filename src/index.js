aa5afa7c2baf33bf47abba3710a0c8abc61854a2
Martin Hertig <martin.hertig@gmail.com>
2020-08-03 17:46:15 +0200

    add mobile navigation

--- src/index.js
+++ src/index.js
@@ -3,7 +3,40 @@
 import "./css/main.css";
 
 
-document.getElementById("build-button").addEventListener("click", showBuildInstructions, false);
+// Mobile nav js
+let menuIsOpen = false
+const menu = document.getElementById('menu')
+const menuBtn = document.getElementById('menu-btn')
+
+const closeMenu = () => {
+  menu.className = menu.className.replace('left-50', 'left-100')
+  menuBtn.innerHTML = 'Menu'
+  menuIsOpen = false
+}
+
+const openMenu = () => {
+  menu.className = menu.className.replace('left-100', 'left-50')
+  menuBtn.innerHTML = 'Close'
+  menuIsOpen = true
+}
+
+window.onload = () => {
+  console.log("Hi there")
+  // bind click handler to menu button
+  menuBtn.onclick = (e) => {
+    e.preventDefault()
+    if (menuIsOpen) closeMenu()
+    else openMenu()
+  }
+}
+
+window.onresize = closeMenu
+
+
+var el = document.getElementById("build-button");
+if (el != null) {
+  el.addEventListener("click", showBuildInstructions, false);
+}
 
 function showBuildInstructions() {
   var x = document.getElementById("build-instructions");
